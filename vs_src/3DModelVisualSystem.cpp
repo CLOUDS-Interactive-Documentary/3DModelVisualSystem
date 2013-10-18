07dc2b9a9a38a97c4a08c07586f2ce0255772156
lars berg <laserstorm@gmail.com>
2013-10-18 10:15:55 -0500

    updated data path for loading arrow meshes

--- vs_src/3DModelVisualSystem.cpp
+++ vs_src/3DModelVisualSystem.cpp
@@ -357,7 +357,7 @@ void CloudsVisualSystem3DModel::selfSetup()
 	loadShaders();
 	
 	//load our non-model meshes
-	ofxObjLoader::load("arrow.obj", arrowMesh, true );
+	ofxObjLoader::load(getVisualSystemDataPath() + "arrow.obj", arrowMesh, true );
 	resizeTheArrowMesh( arrowRadius, arrowHeight, arrowPointHeight );
 	
 	loadCameraLineModel( cameraLines, "cameraVertices.txt" );
